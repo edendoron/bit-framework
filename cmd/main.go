f7b8cffa1991bbaf3355908f582170ec4c561265
NadRose <nadav.rl1@gmail.com>
2021-04-26 12:38:26 +0300

    added waitgroup to main

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,29 @@ import (
 	. "../internal/bitStorageAccess"
 	. "../internal/bitTestResultsExporter"
 	"log"
+	"sync"
 )
 
 func main() {
 	log.Printf("Server started")
-	go BitExporter()
-	go BitStorageAccess()
-	BitIndexer()
-}
 
+	// create a 'WaitGroup'
+	wg := new(sync.WaitGroup)
+	wg.Add(3)
+
+	go func() {
+		BitExporter()
+		wg.Done()
+	}()
+	go func() {
+		BitStorageAccess()
+		wg.Done()
+	}()
+	go func() {
+		BitIndexer()
+		wg.Done()
+	}()
+
+	// wait until WaitGroup is done
+	wg.Wait()
+}

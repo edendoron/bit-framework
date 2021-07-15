37b1c3ccde7fb392822f6be3ab4a6118db6bbd02
NadRose <nadav.rl1@gmail.com>
2021-07-15 13:10:10 +0300

    add ping response values to configuration file

--- internal/bitConfig/api_liveness.go
+++ internal/bitConfig/api_liveness.go
@@ -10,7 +10,7 @@ import (
 
 func ConfigGetPing(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	response := PongBody{Timestamp: time.Now(), Version: "1/00", Host: "EdenNadav", Ready: true, ApiVersion: "alpha"}
+	response := PongBody{Timestamp: time.Now(), Version: Configs.Version, Host: Configs.Host, Ready: true, ApiVersion: Configs.ApiVersion}
 	err := json.NewEncoder(w).Encode(&response)
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)

6d329649bb60f3163711a811c7d9bd0c39c085ad
NadRose <nadav.rl1@gmail.com>
2021-07-17 15:06:44 +0300

    small fixes for UI (#84)

--- website/bit-ui/src/components/StatusTable.tsx
+++ website/bit-ui/src/components/StatusTable.tsx
@@ -10,7 +10,7 @@ import {
     TableCell,
     TableContainer,
     TableHead,
-    TableRow,
+    TableRow, TableSortLabel,
     Typography
 } from "@material-ui/core";
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
@@ -56,9 +56,9 @@ const useRowStyles = makeStyles({
     }
 });
 
-const compareDate = (report1: failureObject, report2: failureObject) => {
-    const Date1 = new Date(report1.timestamp.seconds * 1000).getTime();
-    const Date2 = new Date(report2.timestamp.seconds * 1000).getTime();
+const compareDate = (failure1: failureObject, failure2: failureObject) => {
+    const Date1 = new Date(failure1.timestamp.seconds * 1000).getTime();
+    const Date2 = new Date(failure2.timestamp.seconds * 1000).getTime();
     return Date1 > Date2 ? 1 : -1;
 }
 
@@ -66,8 +66,6 @@ const compareDate = (report1: failureObject, report2: failureObject) => {
 const FailureRow = (failure: failureObject) => {
     const [open, setOpen] = React.useState(false);
     const classes = useRowStyles();
-
-    const timestamp = new Date(failure.timestamp.seconds * 1000).toLocaleString()
 
     return (
         <React.Fragment>
@@ -79,7 +77,6 @@ const FailureRow = (failure: failureObject) => {
                 </TableCell>
                 <TableCell component="th" scope="row">{failure.failure_data.test_id}</TableCell>
                 <TableCell align="center">{failure.failure_data?.test_name}</TableCell>
-                <TableCell align="center">{timestamp}</TableCell>
                 <TableCell align="center">{failure.failure_data.bit_type.map((type) => type + ', ')}</TableCell>
                 <TableCell align="center">{failure.count}</TableCell>
                 <TableCell align="center">{failure.failure_data.severity}</TableCell>
@@ -88,13 +85,20 @@ const FailureRow = (failure: failureObject) => {
                 <TableCell style={{paddingBottom: 0, paddingTop: 0}} colSpan={6}>
                     <Collapse in={open} timeout="auto" unmountOnExit>
                         <Box margin={1}>
-                            <div><span className={classes.tableHeader}>Description:</span> "{failure.failure_data.description}"</div>
+                            <div><span
+                                className={classes.tableHeader}>Description:</span> "{failure.failure_data.description}"
+                            </div>
 
-                            <div><span className={classes.tableHeader}>Additional Info:</span> "{failure.failure_data.additional_info}"</div>
+                            <div><span
+                                className={classes.tableHeader}>Additional Info:</span> "{failure.failure_data.additional_info}"
+                            </div>
 
-                            <div><span className={classes.tableHeader}>Purpose:</span> "{failure.failure_data.purpose}"</div>
+                            <div><span className={classes.tableHeader}>Purpose:</span> "{failure.failure_data.purpose}"
+                            </div>
 
-                            <div><span className={classes.tableHeader}>Operator Failure:</span> "{failure.failure_data.operator_failure}"</div>
+                            <div><span
+                                className={classes.tableHeader}>Operator Failure:</span> "{failure.failure_data.operator_failure}"
+                            </div>
 
                             <div><span className={classes.tableHeader}>Line Replacement Units:</span>
                                 "{failure.failure_data.line_replacent_units.map((unit) => unit + ', ')}"
@@ -111,10 +115,12 @@ const FailureRow = (failure: failureObject) => {
     )
 }
 
-const StatusRow = (props: { status: statusObject, index: number }) => {
+const StatusRow = (props: { status: statusObject, index: number, time: number }) => {
     const {status, index} = props;
     const [open, setOpen] = React.useState(false);
     const classes = useRowStyles();
+
+    const timestamp = new Date(props.time).toLocaleString()
 
     return (
         <React.Fragment>
@@ -125,6 +131,7 @@ const StatusRow = (props: { status: statusObject, index: number }) => {
                     </IconButton>
                 </TableCell>
                 <TableCell component="th" scope="row">{index}</TableCell>
+                <TableCell align="center">{timestamp}</TableCell>
                 <TableCell align="center">{status.failures.length}</TableCell>
             </TableRow>
             <TableRow>
@@ -141,8 +148,6 @@ const StatusRow = (props: { status: statusObject, index: number }) => {
                                             <TableCell/>
                                             <TableCell>ID</TableCell>
                                             <TableCell align="center">Name</TableCell>
-                                            <TableCell align="center">Timestamp</TableCell>
-                                            <TableCell align="center">BIT Type</TableCell>
                                             <TableCell align="center">Failure Count</TableCell>
                                             <TableCell align="center">Severity</TableCell>
                                         </TableRow>
@@ -163,24 +168,104 @@ const StatusRow = (props: { status: statusObject, index: number }) => {
     )
 }
 
+const compareStatusDate = (status1: statusObject, status2: statusObject) => {
+    const Date1 = new Date(status1.failures[0].timestamp.seconds * 1000).getTime();
+    const Date2 = new Date(status2.failures[0].timestamp.seconds * 1000).getTime();
+    return Date1 > Date2 ? 1 : -1;
+}
+
+const compareNumber = (status1: statusObject, status2: statusObject) => {
+    return status1.failures.length > status2.failures.length ? 1 : -1;
+}
+
+function stableSort<T>(array: T[], comparator: (a: T, b: T) => number, direction: string) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+    stabilizedThis.sort((a, b) => {
+        const order = direction == "desc" ? comparator(a[0], b[0]) : -comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+
+const getComparator = (key: string): ((a: statusObject, b: statusObject) => number) => {
+    switch (key) {
+        case "Date":
+            return compareStatusDate
+        case "# of Failures":
+            return compareNumber
+        default:
+            return compareStatusDate
+    }
+}
+
+const useHeaderRowStyles = makeStyles({
+    root: {
+        '& > *': {
+            borderBottom: 'set',
+            backgroundColor: '#b3a48e',
+        },
+    },
+    text: {
+        fontWeight: 'bold',
+    }
+});
 export const StatusTable: FC<StatusTableProps> = ({data}) => {
-    if(data.length === 0) return <div>No Statuses Found.</div>
+    const [orderByField, setOrderField] = React.useState("No.")
+    const [orderByDirection, setOrderDirection] = React.useState<"asc" | "desc">("asc")
+
+    const classes = useHeaderRowStyles();
+
+    if (data.length === 0) return <div>No Statuses Found.</div>
 
     return (
         <TableContainer component={Paper}>
             <Table aria-label="collapsible table">
-                <TableHead>
+                <TableHead className={classes.root}>
                     <TableRow>
                         <TableCell/>
-                        <TableCell>No.</TableCell>
-                        <TableCell align="center"># of Failures</TableCell>
+                        <TableCell className={classes.text}>No.</TableCell>
+                        <TableCell
+                            className={classes.text}
+                            align="center"
+                            sortDirection={false}>
+                            <TableSortLabel
+                                active={orderByField === "Date"}
+                                direction={orderByDirection}
+                                onClick={() => {
+                                    if (orderByDirection === "asc")
+                                        setOrderDirection("desc")
+                                    else
+                                        setOrderDirection("asc")
+                                    setOrderField("Date")
+                                }}>
+                                Timestamp
+                            </TableSortLabel>
+                        </TableCell>
+                        <TableCell
+                            className={classes.text}
+                            align="center"
+                            sortDirection={false}>
+                            <TableSortLabel
+                                active={orderByField === "# of failures"}
+                                direction={orderByDirection}
+                                onClick={() => {
+                                    if (orderByDirection === "asc")
+                                        setOrderDirection("desc")
+                                    else
+                                        setOrderDirection("asc")
+                                    setOrderField("# of failures")
+                                }}>
+                                # of failures
+                            </TableSortLabel>
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {
-                        data.map((status, index) => (
-                            <StatusRow status={status} index={index}/>
-                        ))}
+                    {stableSort(data, getComparator(orderByField), orderByDirection).map((status, index) =>
+                    <StatusRow status={status} index={index} time={status.failures[0].timestamp.seconds * 1000}/>
+                    )}
                 </TableBody>
             </Table>
         </TableContainer>

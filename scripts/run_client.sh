11525dffb3a91ad0cdab2434d2422af86267a41f
NadRose <nadav.rl1@gmail.com>
2021-07-26 11:01:51 +0300

    shell client script

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo Running WEB UI client...
-npm install ./website/bit-ui
-npm start ./website/bit-ui
+npm install --prefix ./website/bit-ui
+npm start --prefix ./website/bit-ui
